b75cbc02bcc8cd7cc8c9b50153d841cab09508eb
hinalmerchant29 <hinalmerchant2911@gmail.com>
2026-04-10 14:03:30 +0530

    Create Assignment12.c

---
+++ Assignment12.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <math.h>
+
+int isPrime(int n) {
+    int i;
+    if(n <= 1)
+        return 0;
+    for(i = 2; i <= n/2; i++) {
+        if(n % i == 0)
+            return 0;
+    }
+    return 1;
+}
+
+long int factorial(int n) {
+    int i;
+    long int fact = 1;
+    for(i = 1; i <= n; i++) {
+        fact *= i;
+    }
+    return fact;
+}
+
+void primeFactors(int n) {
+    int i;
+    printf("Prime factors: ");
+    for(i = 2; i <= n; i++) {
+        while(n % i == 0) {
+            printf("%d ", i);
+            n = n / i;
+        }
+    }
+    printf("\n");
+}
+
+int main() {
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    // a) Square root
+    if(num >= 0)
+        printf("Square root = %.2f\n", sqrt(num));
+    else
+        printf("Square root not defined for negative numbers\n");
+
+    // b) Square
+    printf("Square = %d\n", num * num);
+
+    // c) Cube
+    printf("Cube = %d\n", num * num * num);
+
+    // d) Prime check
+    if(isPrime(num))
+        printf("%d is a Prime number\n", num);
+    else
+        printf("%d is not a Prime number\n", num);
+
+    // e) Factorial
+    if(num >= 0)
+        printf("Factorial = %ld\n", factorial(num));
+    else
+        printf("Factorial not defined for negative numbers\n");
+
+    // f) Prime factors
+    if(num > 1)
+        primeFactors(num);
+    else
+        printf("No prime factors\n");
+
+    return 0;
+}
